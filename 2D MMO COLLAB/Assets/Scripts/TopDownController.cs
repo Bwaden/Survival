60c3b6cac17ddcc06ec3c22520e1a08382611459
BransenJ <bransenjorgensen@gmail.com>
2023-07-11 20:50:23 -0600

    Character Movement
    Character Movement

---
+++ 2D MMO COLLAB/Assets/Scripts/TopDownController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownController : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+
+    public Animator animator;
+
+    public Joystick joystick;
+
+    Vector2 movement;
+
+
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+
+    void Update()
+    {
+
+        movement.x = joystick.Horizontal;
+        movement.y = joystick.Vertical;
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+}
+
