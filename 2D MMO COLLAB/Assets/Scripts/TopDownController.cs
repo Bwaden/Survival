95c269e9114bea9520943ad9dfe30867724d7130
BransenJ <bransenjorgensen@gmail.com>
2023-07-29 22:11:58 -0600

    Cutscene Update

--- 2D MMO COLLAB/Assets/Scripts/TopDownController.cs
+++ 2D MMO COLLAB/Assets/Scripts/TopDownController.cs
@@ -15,6 +15,8 @@ public class TopDownController : MonoBehaviour
 
     Vector2 movement;
 
+    [SerializeField]
+    public float speed;
 
     // Start is called before the first frame update
 
@@ -22,20 +24,28 @@ public class TopDownController : MonoBehaviour
 
     void Update()
     {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
 
-        movement.x = Mathf.Round(joystick.Horizontal);
-        movement.y = Mathf.Round(joystick.Vertical);
-
+    // Normalize the movement vector
+        movement.Normalize();
 
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
-
         animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        if (Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
+        {
+            animator.SetFloat("LastMoveX", Input.GetAxisRaw("Horizontal"));
+            animator.SetFloat("LastMoveY", Input.GetAxisRaw("Vertical"));
+        }
+
     }
+
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+            rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+
     }
 
-}
-
+    }
